85c72e48917a8ed5dcd82667e5a469601792bbf3
Neal Brandi <nealbrandi@yahoo.com>
2016-01-23 17:20:24 -0500

    Initial version

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Component } from 'react';
+
+import BookList from '../containers/bookList';
+import BookDetail from '../containers/bookDetail';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BookList />
+        <BookDetail />
+      </div>
+    );
+  }
+}
---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts', pages: 101},
+    {title: 'Object Oriented Software Construction', pages: 458},
+    {title: 'Spring In Action', pages: 344},
+    {title: 'The Picture Of Dorian Gray', pages: 187},
+    {title: 'American Sniper', pages: 321}
+  ];
+}
